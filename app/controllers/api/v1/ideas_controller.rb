16f35c620bb3a0eab2de7088365769042ba7fa86
Dorje <ku.dorjee@gmail.com>
2017-12-16 11:17:45 +0545

    add create action and render ideas on decs order on created_at

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,8 +1,18 @@
 module Api::V1
   class IdeasController < ApplicationController
     def index
-      @ideas = Idea.all
+      @ideas = Idea.all.order(created_at: :desc);
       render json: @ideas
+    end
+
+    def create
+      idea = Idea.create(idea_params)
+      render json: idea_params
+    end
+
+    private
+    def idea_params
+      params.require(:idea).permit(:title, :body)
     end
   end
 end
