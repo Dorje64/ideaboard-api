d0af37feb7ab937a028fc926af7dbf477a36ac4c
Dorje <ku.dorjee@gmail.com>
2018-01-02 11:37:38 +0545

    add messages controller

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,31 @@
+module Api::V1
+  class MessagesController < ApplicationController
+    before_action :set_user , :set_conversation
+    before_action :set_conversation
+
+    def index
+      # byebug
+      receipts = @conversation.receipts_for(@user).includes(:message)
+      messages = receipts.map{|receipt| receipt.message }
+      render json: messages
+    end
+
+    def create
+      # byebug
+      receipt = @user.reply_to_conversation(@conversation, params[:body])
+
+      render json: receipt.message
+    end
+
+    private
+    def set_conversation
+      @conversation = @user.mailbox.conversations.find_by(id: params[:conversation_id])
+    end
+
+    def set_user
+      @user = User.find_by(email: params[:uid])
+    end
+
+    # def message_params
+  end
+end
