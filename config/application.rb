79667fe68557ba775e0cc842505362a6db994f0e
Dorje <ku.dorjee@gmail.com>
2018-01-03 17:45:23 +0545

    origin config for rack

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Ideaboard
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins '*'
         resource '*',
          :headers => :any,
          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
